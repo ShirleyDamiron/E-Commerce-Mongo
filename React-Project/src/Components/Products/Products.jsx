82484a4c0e869a4dc8ea7dac62940309cc0fb8d7
Shirley Damiron <shirleydamiron@Administrators-MacBook-Pro.local>
2019-11-26 16:32:47 -0500

    updating changes on filter

--- React-Project/src/Components/Products/Products.jsx
+++ React-Project/src/Components/Products/Products.jsx
@@ -67,9 +67,9 @@ class Products extends React.Component {
           </select>
           {/* filter of price */}
           <select className="filterPrice" onChange={this.filterProductsFunc}>
-            <option value="none">Filter By Price</option><label for="none">Filter By Price</label>
-            <option value="high">Highest to Lowest</option><label for="high">Highest to Lowest</label>
-            <option value="low">Lowest to Highest</option><label for="low">Lowest to Highest</label>
+            <option label="price" value="none">Filter By Price</option>
+            <option label="lowest" value="high">Highest to Lowest</option>
+            <option label="highest" value="low">Lowest to Highest</option>
           </select>
         </div>
 
